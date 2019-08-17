d2f6ad9da27bad9b3c5c3a12c67c32265a44bd98
trainocate-repo <51771898+trainocate-repo@users.noreply.github.com>
2019-08-17 13:18:18 +0900

    schema.sql
    不要なテーブルの削除

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,3 @@
-create table student
-(
-   id INT NOT NULL,
-   name varchar(255) not null,
-   passport_number varchar(255) not null,
-   primary key(id)
-);
-
 CREATE TABLE inquiry
 (
    id INT NOT NULL AUTO_INCREMENT,
@@ -22,16 +14,6 @@ CREATE TABLE survey
    age INT NOT NULL,
    satisfaction INT NOT NULL,
    comment VARCHAR(100),
-   created DATETIME NOT NULL,
-   PRIMARY KEY(id)
-);
-
-CREATE TABLE profile
-(
-   id INT NOT NULL AUTO_INCREMENT,
-   profile_id VARCHAR(8) NOT NULL,
-   profile_name VARCHAR(50) NOT NULL,
-   image_name VARCHAR(100),
    created DATETIME NOT NULL,
    PRIMARY KEY(id)
 );
